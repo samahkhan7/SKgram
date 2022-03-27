bc01fe87b6aeb6b83f8e4d304f83a77ac0976fdb
Samah Khan <k.samah7@gmail.com>
2022-03-27 17:52:36 -0500

    Connected to Back4App

--- app/src/main/java/com/example/skgram/MainActivity.kt
+++ app/src/main/java/com/example/skgram/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.skgram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.parse.ParseObject
 
 /**
  * Let user create a post by taking a photo with their camera
@@ -11,5 +13,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
+

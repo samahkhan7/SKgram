3b2904228868a681c3a5fe68c759cb2d6f976a45
Samah Khan <k.samah7@gmail.com>
2022-03-28 00:44:37 -0500

    User is persisted

--- app/src/main/java/com/example/skgram/MainActivity.kt
+++ app/src/main/java/com/example/skgram/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.skgram
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.parse.FindCallback
+import com.parse.ParseException
 import com.parse.ParseObject
+import com.parse.ParseQuery
 
 /**
  * Let user create a post by taking a photo with their camera
@@ -14,7 +17,44 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // 1. setting the description of the past
+
+        // 2. A button to launch the camera to take a picture
+
+        // 3. An ImageView to show the picture the user has taken
+
+        // 4. A button to save and send the post to the Parse server
+
+
+        queryPosts()
+    }
+
+    // Query for all posts in our server
+    fun queryPosts() {
+        // Specify which class to query
+        val query: ParseQuery<Post> = ParseQuery.getQuery(Post::class.java)
+        query.include(Post.KEY_USER)
+        query.findInBackground(object : FindCallback<Post> {
+            override fun done(posts: MutableList<Post>?, e:ParseException?) {
+                if (e != null) {
+                    // something went wrong
+                    Log.e(TAG, "Error fetching posts")
+                } else {
+                    if (posts != null) {
+                        for (post in posts) {
+                            Log.i(TAG,"Post: " + post.getDescription() + " , username: " + post.getUser()?.username)
+
+                        }
+                    }
+                }
+            }
+        })
 
     }
+
+    companion object {
+        const val TAG = "MainActivity"
+    }
+
 }
 
